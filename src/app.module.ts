a9d942971d66c73c94a0e798af33f5e51900a237
Victor Zvonarev <kros1.ty@gmail.com>
2023-05-16 21:39:04 +0300

    Post CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostModule } from './post/post.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmAsyncConfig } from './config/typeorm.config';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PostModule } from './post/post.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { typeOrmAsyncConfig } from './config/typeorm.config'
 
 @Module({
-  imports: [
-    PostModule,
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		PostModule,
+		ConfigModule.forRoot({
+			envFilePath: '.env',
+			isGlobal: true
+		}),
+		TypeOrmModule.forRootAsync(typeOrmAsyncConfig)
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,29 +1,48 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
-import { PostRepository } from './repository/post.repository';
+import { Injectable, NotFoundException, Param } from '@nestjs/common'
+import { CreatePostDto } from './dto/create-post.dto'
+import { UpdatePostDto } from './dto/update-post.dto'
+import { PostRepository } from './repository/post.repository'
+import { Post as PostEntity } from './entities/post.entity'
 
 @Injectable()
 export class PostService {
-  constructor(private readonly postRepository: PostRepository) {}
+	constructor(private readonly postRepository: PostRepository) {}
 
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
+	create(createPostDto: CreatePostDto): Promise<PostEntity> {
+		const newPost = this.postRepository.create(createPostDto)
+		return this.postRepository.save(newPost)
+	}
 
-  findAll() {
-    return this.postRepository.find();
-  }
+	findAll() {
+		return this.postRepository.find()
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
+	async findOne(@Param('id') id: string): Promise<PostEntity[]> {
+		const post = await this.postRepository.findBy({ id })
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
+		if (!post.length) {
+			throw new NotFoundException('Post not found')
+		}
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
-  }
+		return post
+	}
+
+	async update(id: string, updatePostDto: UpdatePostDto): Promise<PostEntity> {
+		const oldPost = await this.findOne(id)
+
+		if (!oldPost.length) {
+			throw new NotFoundException('Post not found')
+		}
+
+		const editedPost = { ...oldPost[0], ...updatePostDto }
+		return await this.postRepository.save(editedPost)
+	}
+
+	async remove(@Param('id') id: string): Promise<void> {
+		const result = await this.postRepository.delete(id)
+
+		if (result.affected === 0) {
+			throw new NotFoundException(`Post ${id} not exists`)
+		}
+	}
 }
