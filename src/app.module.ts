85462e0e66a987c1507e85526d2fad32c15d84e0
Victor Zvonarev <kros1.ty@gmail.com>
2023-05-16 16:45:00 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+
+@Module({
+  imports: [
+    PostModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
