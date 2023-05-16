f35621c7e45f27bd5ba97ef640a4fb0332e5908c
Victor Zvonarev <kros1.ty@gmail.com>
2023-05-16 17:11:12 +0300

    add post entity

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,17 @@
-export class Post {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { PostStatus } from '../post-status.enum';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  body: string;
+
+  @Column()
+  status: PostStatus;
+}
